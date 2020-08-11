237a6f09a60e517370a716aa4e8155d5e0d1141b
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-08-11 09:47:53 +0530

    Add files via upload

---
+++ reference/event_demo.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+//create Class
+class MyEmitter extends EventEmitter { }
+
+//Init object 
+const myEmitter = new MyEmitter();
+
+// Event listener
+myEmitter.on('event',()=> console.log('Event Fired !'));
+
+//Init event 
+myEmitter.emit('event');
+myEmitter.emit('event');
+myEmitter.emit('event');
+myEmitter.emit('event');
